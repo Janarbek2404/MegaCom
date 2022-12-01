10f424491895ca56bc35887bac4822e602bfc1e7
Janarbek <Janar24kalmyk@gmail.com>
2022-12-01 19:59:50 +0600

    react

---
+++ React/29-11/my-app/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+
+const Header =()=>{
+    return(
+        <div>
+            <h1>Hello Header</h1>
+        </div>
+    )
+}
+
+export default App;
